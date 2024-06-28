40dba42a6f252063f158286d8bfd6c176f1a1ac2
bongumousa <mkhize.bongumusa@gmail.com>
2024-06-28 10:19:46 +0200

    remove firebase configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCohUb4lUmY7xPEM6wEBy5MdeDG_eOOIKw",
-  authDomain: "pingembo-721f1.firebaseapp.com",
-  databaseURL: "https://pingembo-721f1-default-rtdb.firebaseio.com",
-  projectId: "pingembo-721f1",
-  storageBucket: "pingembo-721f1.appspot.com",
-  messagingSenderId: "1079845605013",
-  appId: "1:1079845605013:web:d6f8161e000679bbead59c",
-  measurementId: "G-B91R7TV3LJ"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCohUb4lUmY7xPEM6wEBy5MdeDG_eOOIKw",
+//   authDomain: "pingembo-721f1.firebaseapp.com",
+//   databaseURL: "https://pingembo-721f1-default-rtdb.firebaseio.com",
+//   projectId: "pingembo-721f1",
+//   storageBucket: "pingembo-721f1.appspot.com",
+//   messagingSenderId: "1079845605013",
+//   appId: "1:1079845605013:web:d6f8161e000679bbead59c",
+//   measurementId: "G-B91R7TV3LJ"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 @NgModule({ 
   declarations: [
