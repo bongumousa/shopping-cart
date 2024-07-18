f17252b2f9cd3be913d7027a482457edf37b5fa5
LungstarIQ <thandananimkhize538@gmail.com>
2024-07-18 21:51:10 +0200

    Updated one item using one way data binding.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  name = 'Mustang';
+  model = 'American';
+  description = 'Standard on all 2025 Mustang® EcoBoost® trims and available on all others, this smooth-shifting transmission comes with available steering wheel-mounted paddle shifters.';
+  imageF = 'https://www.thedrive.com/wp-content/uploads/2023/12/03/mustang-dark-horse-1.jpg?w=1173&h=660';
+  imageB = 'https://cdn.motor1.com/images/mgl/npP3k/s3/2021-ford-mustang-mach-1.webp';
+  link = 'https://www.thedrive.com/news/current-ford-mustang-production-is-ending-in-2028-report';
+  price = '$1200000';
+  
 }
